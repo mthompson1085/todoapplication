8605377a984f4dfe23d840f6eb774da1eb022c6d
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-09-06 09:57:10 -0400

    updated compenent name

--- todo-app/src/componets/visibilityFilter.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function VisibilityFilter({ filters }) {
-    return (
-        filters.map((filter,i) => <button key={`filter-${i}`}>{filter}</button>)
-    )
-}
