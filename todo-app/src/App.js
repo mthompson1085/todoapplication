e839b23bc13e21f7bc597323307888b71cb98445
mthompson1085 <65202594+mthompson1085@users.noreply.github.com>
2020-09-06 12:20:02 -0400

    updated app js to render todo list filters

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import AddTodo from './components/addTodo'
 import TodoList from './components/todoList'
 import VisibilityFilter from './components/visibilityFilter'
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
-const filters = ['all', 'completed', 'incomplete']
+
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <h1>Financial Aid To Do's!</h1>
       <AddTodo/>
       <TodoList todos={[{content: 'Gather Tax Information'}, {content: 'Apply for FSA ID'}]}/>
-      <VisibilityFilter filters={filters}/>
+      <VisibilityFilter/>
     </div>
+    </Provider>
   );
 }
 
